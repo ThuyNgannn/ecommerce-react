2e8e03695bf54f4660057da9072a93ffe8548f36
ThaiPhu2k4 <132972807+ThaiPhu2k4@users.noreply.github.com>
2024-07-20 21:12:48 +0700

    Update App.js
    sua

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,32 @@
 import React from "react";
-import { RouterProvider } from "react-router-dom";
-import Client from "./pages/client";
-import Admin from "./pages/admin"
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AdminPage from "./pages/admin/src/App";
+// import ClientPage from "./pages/client";
+
+const adminRouter = createBrowserRouter([
+  {
+    path: "/admin",
+    element: <AdminPage />,
+  },
+]);
+
+// const clientRouter = createBrowserRouter([
+//   {
+//     path: "/client",
+//     element: <ClientPage />,
+//   },
+// ]);
+
 function App() {
-  return (
-    <React.StrictMode>
-      <RouterProvider router={Client} />
-    </React.StrictMode>
-  );
+  // Example: switching based on some condition
+  const isAdmin = true; // Replace with actual condition
+  // const router = isAdmin ? adminRouter : clientRouter;
+
+  // return (
+  //   <React.StrictMode>
+  //     <RouterProvider router={router} />
+  //   </React.StrictMode>
+  // );
 }
 
 export default App;
-
