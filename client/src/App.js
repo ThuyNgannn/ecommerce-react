296c5980e9ad1d2473af0926fc1496d67693ef83
ThaiPhu2k4 <132972807+ThaiPhu2k4@users.noreply.github.com>
2024-07-21 17:44:51 +0700

    phu
    sửa

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,1 @@
-import React from "react";
-import { RouterProvider } from "react-router-dom";
-import Client from "./pages";
-import RouterAdmin from "./pages/admin/src";
 
-function App() {
-  return (
-    <React.StrictMode>
-      <RouterProvider router={Client} />
-      <RouterAdmin />
-    </React.StrictMode>
-  );
-}
-
-export default App;
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,11 +1,1 @@
-import React from "react";
-import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
-import Home from './Home/Home';
 
-const Client = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<Home />} />
-  )
-);
-
-export default Client;
